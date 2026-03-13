0c5f9fdd3dc0c427e9e93e65ec4b88b0faf82ea4
CyberVortex20 <tiwarishashvat1@gmail.com>
2026-03-13 15:02:12 +0530

    Shashvat Added UseCase13PalindromeCheckerApp

--- App/src/UseCase11PalindromeCheckerApp.java
+++ App/src/UseCase11PalindromeCheckerApp.java
@@ -3,67 +3,72 @@ import java.util.Stack;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-interface PalindromeStrategy {
-    boolean checkPalindrome(String str);
-}
+public class UseCase13PalindromeCheckerApp {
 
-class StackStrategy implements PalindromeStrategy {
-    public boolean checkPalindrome(String str) {
-        str = str.replaceAll("\\s+", "").toLowerCase();
+    public static boolean stackPalindrome(String str) {
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++)
             stack.push(str.charAt(i));
-        }
 
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++)
             if (str.charAt(i) != stack.pop())
                 return false;
-        }
 
         return true;
     }
-}
 
-class DequeStrategy implements PalindromeStrategy {
-    public boolean checkPalindrome(String str) {
-        str = str.replaceAll("\\s+", "").toLowerCase();
+    public static boolean dequePalindrome(String str) {
         Deque<Character> deque = new ArrayDeque<>();
 
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++)
             deque.addLast(str.charAt(i));
-        }
 
-        while (deque.size() > 1) {
+        while (deque.size() > 1)
             if (deque.removeFirst() != deque.removeLast())
                 return false;
+
+        return true;
+    }
+
+    public static boolean twoPointerPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+            start++;
+            end--;
         }
 
         return true;
     }
-}
 
-public class UseCase12PalindromeCheckerApp {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        System.out.print("Choose Strategy (1-Stack, 2-Deque): ");
-        int choice = sc.nextInt();
+        input = input.replaceAll("\\s+", "").toLowerCase();
 
-        PalindromeStrategy strategy;
+        long start1 = System.nanoTime();
+        boolean result1 = stackPalindrome(input);
+        long end1 = System.nanoTime();
 
-        if (choice == 1)
-            strategy = new StackStrategy();
-        else
-            strategy = new DequeStrategy();
+        long start2 = System.nanoTime();
+        boolean result2 = dequePalindrome(input);
+        long end2 = System.nanoTime();
 
-        if (strategy.checkPalindrome(input))
-            System.out.println("The given string is a Palindrome.");
-        else
-            System.out.println("The given string is NOT a Palindrome.");
+        long start3 = System.nanoTime();
+        boolean result3 = twoPointerPalindrome(input);
+        long end3 = System.nanoTime();
+
+        System.out.println("Stack Method Result: " + result1 + " Time: " + (end1 - start1) + " ns");
+        System.out.println("Deque Method Result: " + result2 + " Time: " + (end2 - start2) + " ns");
+        System.out.println("Two Pointer Method Result: " + result3 + " Time: " + (end3 - start3) + " ns");
 
         sc.close();
     }
